8720340e07ecf024e4505ed9caacdde2ccb14aa2
chonsawat <chonsawat.nakanam@kkumail.com>
2023-04-04 01:42:27 +0700

    Add project files.

---
+++ Todolist - ASP.NET (MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Todolist___ASP.NET__MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
